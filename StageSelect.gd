2e4217f90e3370ef01438d4e8dec656e6fce537c
Rando2020 <75230570+Rando2020@users.noreply.github.com>
2026-05-19 22:39:15 -0500

    Add files via upload

---
+++ StageSelect.gd
@@ -0,0 +1,35 @@
+; Engine configuration file.
+; It's best edited using the editor UI and not directly,
+; since the parameters that go here are not all obvious.
+;
+; Format:
+;   [section] ; section goes between []
+;   param=value ; assign values to parameters
+
+config_version=5
+
+[animation]
+
+compatibility/default_parent_skeleton_in_mesh_instance_3d=true
+
+[application]
+
+config/name="ProjectTactic"
+config/description="Tactical RPG - Godot 4 Implementation"
+config/version="0.1.0"
+config/tags=PackedStringArray("tactical", "rpg", "turn-based", "roguelite")
+run/main_scene="res://scenes/HubScene.tscn"
+config/features=PackedStringArray("4.6", "Forward Plus")
+config/icon="res://icon.svg"
+
+[autoload]
+
+VFX="*res://scripts/vfx/VFXManager.gd"
+GameState="*res://scripts/systems/GameState.gd"
+RunManager="*res://scripts/roguelite/RunManager.gd"
+
+[display]
+
+window/size/viewport_width=1280
+window/size/viewport_height=720
+window/stretch/mode="canvas_items"
